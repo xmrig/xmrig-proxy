1510b4ba3c6f4968ab8941733c8a4d98e0a706f2
XMRig <support@xmrig.com>
2019-02-08 16:41:00 +0700

    Sync changes.

--- src/net/strategies/DonateStrategy.h
+++ src/net/strategies/DonateStrategy.h
@@ -5,7 +5,8 @@
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
- * Copyright 2016-2018 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2018-2019 SChernykh   <https://github.com/SChernykh>
+ * Copyright 2016-2019 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -21,12 +22,13 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __DONATESTRATEGY_H__
-#define __DONATESTRATEGY_H__
+#ifndef XMRIG_DONATESTRATEGY_H
+#define XMRIG_DONATESTRATEGY_H
 
 
 #include "common/interfaces/IClientListener.h"
 #include "common/interfaces/IStrategy.h"
+#include "common/net/Job.h"
 #include "common/utils/c_str.h"
 
 
@@ -52,11 +54,11 @@ public:
 
 
     DonateStrategy(xmrig::Controller *controller, IStrategyListener *listener);
-    ~DonateStrategy();
+    ~DonateStrategy() override;
 
     bool reschedule();
     void save(const Client *client, const Job &job);
-    void setAlgo(const xmrig::Algorithm &algorithm);
+    void setAlgo(const xmrig::Algorithm &algorithm) override;
 
     inline bool hasPendingJob() const     { return m_pending.job.isValid(); }
     inline const Pending &pending() const { return m_pending; }
@@ -86,4 +88,4 @@ private:
     xmrig::Controller *m_controller;
 };
 
-#endif /* __SINGLEPOOLSTRATEGY_H__ */
+#endif /* XMRIG_DONATESTRATEGY_H */

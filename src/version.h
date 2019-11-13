054ecc64dfee4229d68c0d96e25f3b9b517a03ce
XMRig <support@xmrig.com>
2019-11-13 13:44:05 +0700

    v5.0.0-dev

--- src/version.h
+++ src/version.h
@@ -28,14 +28,14 @@
 #define APP_ID        "xmrig-proxy"
 #define APP_NAME      "xmrig-proxy"
 #define APP_DESC      "XMRig Stratum proxy"
-#define APP_VERSION   "4.5.0-beta"
+#define APP_VERSION   "5.0.0-dev"
 #define APP_DOMAIN    "xmrig.com"
 #define APP_SITE      "www.xmrig.com"
 #define APP_COPYRIGHT "Copyright (C) 2016-2019 xmrig.com"
 #define APP_KIND      "proxy"
 
-#define APP_VER_MAJOR  4
-#define APP_VER_MINOR  5
+#define APP_VER_MAJOR  5
+#define APP_VER_MINOR  0
 #define APP_VER_PATCH  0
 
 #ifdef _MSC_VER

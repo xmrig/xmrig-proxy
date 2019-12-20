0581513570ace4832d5757117e1136456bd65f6f
XMRig <support@xmrig.com>
2019-12-20 19:55:15 +0700

    Added RandomV support.

--- src/crypto/common/Algorithm.cpp
+++ src/crypto/common/Algorithm.cpp
@@ -114,6 +114,8 @@ static AlgoName const algorithm_names[] = {
     { "RandomARQ",                 nullptr,            Algorithm::RX_ARQ          },
     { "randomx/sfx",               "rx/sfx",           Algorithm::RX_SFX          },
     { "RandomSFX",                 nullptr,            Algorithm::RX_SFX          },
+    { "randomx/v",                 "rx/v",             Algorithm::RX_V            },
+    { "RandomV",                   nullptr,            Algorithm::RX_V            },
 #   endif
 #   ifdef XMRIG_ALGO_ARGON2
     { "argon2/chukwa",             nullptr,            Algorithm::AR2_CHUKWA      },
@@ -141,6 +143,7 @@ size_t xmrig::Algorithm::l2() const
     case RX_0:
     case RX_LOKI:
     case RX_SFX:
+    case RX_V:
         return 0x40000;
 
     case RX_WOW:
@@ -177,6 +180,7 @@ size_t xmrig::Algorithm::l3() const
         case RX_0:
         case RX_LOKI:
         case RX_SFX:
+        case RX_V:
             return oneMiB * 2;
 
         case RX_WOW:
@@ -277,6 +281,7 @@ xmrig::Algorithm::Family xmrig::Algorithm::family(Id id)
     case RX_LOKI:
     case RX_ARQ:
     case RX_SFX:
+    case RX_V:
         return RANDOM_X;
 #   endif
 

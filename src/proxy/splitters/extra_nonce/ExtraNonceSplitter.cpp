681abb8e79438b2cd38160ec9f1f78898e353fb7
XMRig <support@xmrig.com>
2021-07-06 00:42:07 +0700

    Fixed donate over proxy in extra nonce mode.

--- src/proxy/splitters/extra_nonce/ExtraNonceSplitter.cpp
+++ src/proxy/splitters/extra_nonce/ExtraNonceSplitter.cpp
@@ -5,8 +5,8 @@
  * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
- * Copyright 2018-2020 SChernykh   <https://github.com/SChernykh>
- * Copyright 2016-2020 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2018-2021 SChernykh   <https://github.com/SChernykh>
+ * Copyright 2016-2021 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -160,11 +160,19 @@ void xmrig::ExtraNonceSplitter::login(LoginEvent *event)
 
 void xmrig::ExtraNonceSplitter::remove(Miner *miner)
 {
+    if (miner->mapperId() < 0 || miner->routeId() != -1) {
+        return;
+    }
+
     m_upstream->remove(miner);
 }
 
 
 void xmrig::ExtraNonceSplitter::submit(SubmitEvent *event)
 {
+    if (event->miner()->mapperId() < 0 || event->miner()->routeId() != -1) {
+        return;
+    }
+
     m_upstream->submit(event);
 }

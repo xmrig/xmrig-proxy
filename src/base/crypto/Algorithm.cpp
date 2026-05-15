264e19f40c8dd45a8d0d124a9bbb7a9b428d682d
Kasemcyber <43542487+Kasemcyber@users.noreply.github.com>
2026-05-15 22:34:00 +0700

    Add support for RX_C64 algorithm in Algorithm.cpp

--- src/base/crypto/Algorithm.cpp
+++ src/base/crypto/Algorithm.cpp
@@ -83,6 +83,7 @@ const char *Algorithm::kRX_ARQ          = "rx/arq";
 const char *Algorithm::kRX_GRAFT        = "rx/graft";
 const char *Algorithm::kRX_SFX          = "rx/sfx";
 const char *Algorithm::kRX_YADA         = "rx/yada";
+const char *Algorithm::kRX_C64          = "rx/c64";
 #endif
 
 #ifdef XMRIG_ALGO_ARGON2
@@ -150,6 +151,7 @@ static const std::map<uint32_t, const char *> kAlgorithmNames = {
     ALGO_NAME(RX_GRAFT),
     ALGO_NAME(RX_SFX),
     ALGO_NAME(RX_YADA),
+    ALGO_NAME(RX_C64),
 #   endif
 
 #   ifdef XMRIG_ALGO_ARGON2
@@ -259,7 +261,6 @@ static const std::map<const char *, Algorithm::Id, aliasCompare> kAlgorithmAlias
                                     ALGO_ALIAS(RX_V2,           "rx/v2"),
     ALGO_ALIAS_AUTO(RX_WOW),        ALGO_ALIAS(RX_WOW,          "randomx/wow"),
                                      ALGO_ALIAS(RX_WOW,          "randomwow"),
-                                     ALGO_ALIAS(RX_WOW,          "rx/c64"),
     ALGO_ALIAS_AUTO(RX_ARQ),        ALGO_ALIAS(RX_ARQ,          "randomx/arq"),
                                     ALGO_ALIAS(RX_ARQ,          "randomarq"),
     ALGO_ALIAS_AUTO(RX_GRAFT),      ALGO_ALIAS(RX_GRAFT,        "randomx/graft"),
@@ -267,7 +268,10 @@ static const std::map<const char *, Algorithm::Id, aliasCompare> kAlgorithmAlias
     ALGO_ALIAS_AUTO(RX_SFX),        ALGO_ALIAS(RX_SFX,          "randomx/sfx"),
                                     ALGO_ALIAS(RX_SFX,          "randomsfx"),
     ALGO_ALIAS_AUTO(RX_YADA),       ALGO_ALIAS(RX_YADA,         "randomx/yada"),
-                                    ALGO_ALIAS(RX_YADA,         "randomyada"),
+                                     ALGO_ALIAS(RX_YADA,         "randomyada"),
+    ALGO_ALIAS_AUTO(RX_C64),        ALGO_ALIAS(RX_C64,          "rx/c64"),
+                                     ALGO_ALIAS(RX_C64,          "randomx/c64"),
+                                     ALGO_ALIAS(RX_C64,          "randomc64"),
 #   endif
 
 #   ifdef XMRIG_ALGO_ARGON2
@@ -355,7 +359,7 @@ std::vector<xmrig::Algorithm> xmrig::Algorithm::all(const std::function<bool(con
         CN_HEAVY_0, CN_HEAVY_TUBE, CN_HEAVY_XHV,
         CN_PICO_0, CN_PICO_TLO,
         CN_UPX2,
-        RX_0, RX_V2, RX_WOW, RX_ARQ, RX_GRAFT, RX_SFX, RX_YADA,
+        RX_0, RX_V2, RX_WOW, RX_ARQ, RX_GRAFT, RX_SFX, RX_YADA, RX_C64,
         AR2_CHUKWA, AR2_CHUKWA_V2, AR2_WRKZ,
         KAWPOW_RVN,
         GHOSTRIDER_RTM
